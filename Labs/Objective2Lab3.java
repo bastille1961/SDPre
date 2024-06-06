b151a39bd8553fee855a6a9ed423b195ab8c5c8d
johnroberts <bastille1961@gmail.com>
2024-06-06 15:01:26 -0400

    Obj2Lab3 Declare Vars

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3.java {
+	public static void main(String[] args) {
+		int birthYear = "1961";
+		String birthMonth = "July";
+		Boolean todayWeekend = "false";
+		System.out.println(birthYear);
+		System.out.println(birthMonth);
+		System.out.println(todayWeekend);
+
+	}
+
+}

0649cf727902319ffc767a9b0e7e75028fb75702
johnroberts <bastille1961@gmail.com>
2024-06-06 14:46:21 -0400

    Obj2Lab2 Print mult vars

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+			String name = "John Roberts";
+			int age = 62;
+			String state = "Pennsylvania";
+			System.out.println(name);\break;System.outprintln(age);\b;System.out.println(state);
+
+	}
+
+}

b2ebe69789e09ee231f2db58211540574d476dae
johnroberts <bastille1961@gmail.com>
2024-06-07 16:58:28 -0400

    Obj3Lab2 Right Triangle determine Hypotenuse

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String [] args) {
+		double base_leg = 4;
+		double vertical_leg = 8;
+		base_leg = base_leg * base_leg;
+		vertical_leg = vertical_leg * vertical_leg;
+		double both_legs = base_leg + vertical_leg;
+		double hypotenuse = Math.sqrt(both_legs);
+		System.out.println("The value of the hypotenuse is:" + hypotenuse);
+
+	}
+
+}

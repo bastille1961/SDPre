0a954574e4a96e87d8eccd334044d187e4f7d92b
johnroberts <bastille1961@gmail.com>
2024-06-10 08:36:05 -0400

    Obj7Lab4 Sum Nbrs

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+	public static void main(String [] args) {
+
+		int currSum = 0;
+		int currCount = 1;
+		while (count <= 20) {
+			currSum += currCount;
+			currCount + 1;
+
+		}
+
+		System.out.println(currSum);
+	}
+
+}
